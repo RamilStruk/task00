0804603d90055bb86db8609d71ee56e6f1f8e10f
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-07 19:17:49 +0800

    task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу, которая возводит в квадрат вводимое число
+Console.WriteLine("введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"квадрат числа {number}={square}");
