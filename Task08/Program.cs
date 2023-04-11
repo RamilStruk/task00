d4e17266550025e4faef1c58c161303ef7219bbd
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-12 02:21:52 +0800

    task 8

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int number2 = 2;
+
+if (number > 1)
+{
+    while (number2 <= number)
+    {
+        Console.Write(number2 + " ");
+        number2 = number2 + 2;
+    }
+}
