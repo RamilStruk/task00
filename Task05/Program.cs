9f504519b62e868aa51d1b4ff9300d66f5b52e86
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-12 01:12:05 +0800

    task 3

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("введите число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int count = -number1;
-while (count <= number1) ;
+while (count <= number1) 
 {
     Console.WriteLine(count + " ");
     count++;
