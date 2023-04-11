eb407badfdaf3df4649c5e0f7eede5662fee8606
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-12 02:08:00 +0800

    task 4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+int max = 0;
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > max)
+{
+    max = number1;
+}
+if(number2 > max)
+{
+    max = number2;
+}
+if(number3 > max)
+{
+    max = number3;
+}
+
+Console.WriteLine("max = " + max);
