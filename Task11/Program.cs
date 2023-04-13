4bf9f9da2f5ea8d78a756d55b0205f8bd8685b5e
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-14 01:22:38 +0800

    task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿//выводит случайное трехзначное число и
+// удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 999);
+Console.WriteLine($"случайное трехзначное число{number}");
+
+int DeleteCenter (int num)
+{
+    int first = num / 100;
+    int third = num % 10;
+    int answer = first * 10 + third;
+    return answer;
+}
+Console.WriteLine($"двузначное число{DeleteCenter(number)}");
