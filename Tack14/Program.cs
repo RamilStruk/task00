81bc022dd3301cc77f30efd5c4278d1a98924cec
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-14 02:33:46 +0800

    task 12/1

---
+++ Tack14/Program.cs
@@ -0,0 +1,15 @@
+﻿//проверяет, делится ли число одновременно на 7 и 23
+
+
+
+
+
+Console.Writeline("введите число ");
+int number = Convert.ToInt32(Console.Readline());
+bool result = MultipleTwoDigits(number);
+Console.WriteLine(result ? "да" : "нет");
+bool MultipleTwoDigits(int num);
+{
+    return num % 7 == 0 && num % 23 == 0;
+
+}
