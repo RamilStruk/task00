06d1bc9aae134436465e6c7e1856cc8ecee7195a
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-14 03:00:49 +0800

    task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿//принимает два числа и проверяет, является ли одно квадратом другого
+
+Console.WriteLine("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+bool result = isSquare(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
+
+bool isSquare(int num1, int num2)
+{
+    return (num1 == num2 + num2) || (num2 == num1 + num1);
+}
