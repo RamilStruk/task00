3e0f999c4f06f4d9c513ec35e5f799dde374d493
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-14 02:47:46 +0800

    task 14

--- Tack14/Program.cs
+++ Tack14/Program.cs
@@ -4,12 +4,12 @@
 
 
 
-Console.Writeline("введите число ");
-int number = Convert.ToInt32(Console.Readline());
-bool result = MultipleTwoDigits(number);
+Console.Write("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = MultipleTwoDigits(number, 7, 23);
 Console.WriteLine(result ? "да" : "нет");
-bool MultipleTwoDigits(int num);
+bool MultipleTwoDigits(int num, int num1, int num2)
 {
-    return num % 7 == 0 && num % 23 == 0;
+    return num % num1 == 0 && num % num2 == 0;
 
 }
