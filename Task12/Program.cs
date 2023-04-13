ba866cdf511c7616f1032ac5c369772d44dbd0e2
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-14 02:02:25 +0800

    task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿//принимает два числа, и выводит, является 
+//ли первое число кратно второму.
+//если нет - выводится остаток деления
+
+Console.WriteLine("введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = Remainder (numberA ,numberB);
+
+if (result == 0)
+{
+    Console.Write("кратно");
+}
+else
+{
+    Console.Write($"не кратно, остаток {result}");
+}
+
+int Remainder(int numA, int numB)
+{
+    return numA % numB;
+}
