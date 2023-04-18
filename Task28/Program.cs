5d27ca5c927514a7958fbe7abd00c3bb442eb6c3
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-18 20:26:12 +0800

    task 30

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите целое положительное число:");
+﻿/* Задача 28: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+
+Console.WriteLine("Введите целое положительное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = NumbersMultiplication(number);
