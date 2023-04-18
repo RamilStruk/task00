f7afe07dfa864054162a0457aa9e703afa4cefda
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-18 19:22:50 +0800

    task 28

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+int DigitCount (int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        sum++;
+    }
+        return sum;
+}
+
+Console.WriteLine("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitCount = DigitCount(number);
+Console.WriteLine($"{digitCount}");
---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumbersMultiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+int NumbersMultiplication(int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            res *= i; // res = res * i;
+        }
+    }
+    return res;
+}
