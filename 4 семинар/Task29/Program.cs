f6468e907eb86cf8c94d5101526bb9de2e64ad70
Рамиль Стрюк <124810588+RamilStruk@users.noreply.github.com>
2023-04-25 00:32:21 +0800

    дз.Task29

---
+++ 4 семинар/Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33]
+ */
+
+Console.WriteLine("Введите длину массива");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+Console.Write($"->  [ ");
+PrintArray(arr);
+Console.Write($"] ");
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    Random random = new Random(); // оптимизация объекта Random (var - объект неявной типизации)
+    for (int i = 0; i < lenghtArray; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
